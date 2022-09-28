44f0ea6e2074fb0cf974c1f93f39e4a8113cdd5b
alessandro-cav <alessandrocav14@gmail.com>
2022-09-28 18:21:52 -0300

    Tratamenmto de exceções e etc

---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/erudio/MathController.kt
@@ -0,0 +1,76 @@
+package br.com.erudio
+
+import br.com.erudio.converters.NumberConverter
+import br.com.erudio.exceptions.UnsupportedMathOperationException
+import br.com.erudio.math.SimpleMath
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.atomic.AtomicLong
+
+@RestController
+class MathController {
+
+    val counter: AtomicLong = AtomicLong()
+
+
+    // Ennsinando como pegar o paramentro na url usando o  @PathVariable que é obrigatorio colocar o valor
+    // Criando a url que vai receber o valor, ficaria assim por exemplo na web localhost:8080/sum/1/2
+    // Double na assinatura da fun seria o tipo de  retorno da mesma
+
+    private val math: SimpleMath = SimpleMath()
+
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    fun sum(@PathVariable(value="numberOne") numberOne: String?,
+            @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    }
+
+    @RequestMapping(value = ["/subtraction/{numberOne}/{numberTwo}"])
+    fun subtraction(@PathVariable(value="numberOne") numberOne: String?,
+                    @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    }
+
+    @RequestMapping(value = ["/multiplication/{numberOne}/{numberTwo}"])
+    fun multiplication(@PathVariable(value="numberOne") numberOne: String?,
+                       @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    }
+
+    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
+    fun division(@PathVariable(value="numberOne") numberOne: String?,
+                 @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    }
+
+    @RequestMapping(value = ["/mean/{numberOne}/{numberTwo}"])
+    fun mean(@PathVariable(value="numberOne") numberOne: String?,
+             @PathVariable(value="numberTwo") numberTwo: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.mean(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
+    }
+
+    @RequestMapping(value = ["/squareRoot/{number}"])
+    fun squareRoot(@PathVariable(value="number") number: String?
+    ): Double {
+        if (!NumberConverter.isNumeric(number))
+            throw UnsupportedMathOperationException("Please set a numeric value!")
+        return math.squareRoot(NumberConverter.convertToDouble(number))
+    }
+}
